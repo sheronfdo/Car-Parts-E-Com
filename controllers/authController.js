5f5a1b5311bd19dcffed3cc491913cd80aed142b
sheronfdo <jamithsheron5@gmail.com>
2025-04-17 01:23:38 +0530

    Enhance login response: Include user status in the response data for better client-side handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,7 +75,7 @@ exports.login = async (req, res) => {
 
         res.json({
             success: true,
-            data: {userId: user._id, token, role : user.role}
+            data: {userId: user._id, token, role : user.role, status: user.status}
         });
     } catch (err) {
         res.status(500).json({success: false, message: err.message});
